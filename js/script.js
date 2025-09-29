f8fc88618ccf93487a1bff2e1e23b931937da965
MD2020m <dmarshall2019@gmail.com>
2025-09-28 20:17:44 -0500

    Add logic to highlight selected responses and record user responses

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("script.js connected!");
+// Create an object to track user responses in
+let responses = {};
+
+// Select each of the four question blocks in the html document
+let questions = document.querySelectorAll(".question-container")
+// Record id and select each answer choice for each question
+questions.forEach(function(question) {
+    let questionID = question.id;
+    let answers = question.querySelectorAll("button")
+    // Add an event listener to each answer choice
+    answers.forEach(function(ans) {
+        // Highlight selected answer and remove highlight from any unselected responses
+        ans.addEventListener("click", function() {
+            answers.forEach(function(btn) {
+                btn.classList.remove("selected");
+            })
+        ans.classList.add("selected");
+        // Record response to score later
+        responses[questionID] = ans.dataset.answer;
+        });
+    });
+});
